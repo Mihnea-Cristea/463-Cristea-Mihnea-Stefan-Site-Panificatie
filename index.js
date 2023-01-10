1b82aa3015bf094ff9bc86b0bd7a6778d798481d
Mihnea-Cristea <mihnea-stefan.cristea@my.fmi.unibuc.ro>
2023-01-10 22:33:49 +0200

    Update

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const fs = require("fs");
 const sharp = require("sharp");
+const { Client } = require("pg");
+
+var client = new Client({
+    database: "prajituri",
+    user: "miki",
+    password: "1234",
+    host: "localhost",
+    port: 5432
+});
+
+// pentru testare functionare DB
+// client.query("select * from prajituri", function (err, rez) {
+//     if (err)
+//         console.log(err);
+//     else
+//         console.log(rez);
+// });
+
+client.connect();
+
 
 app = express();
 
@@ -12,6 +32,22 @@ obGlobal = {
     erori: null,
     imagini: null
 }
+
+var vedeToataLumea="ceva!";
+app.use("/*",function(req,res,next){
+    res.locals.vede=vedeToataLumea;
+    next();
+})
+// app.use("/", function (req, res, next) {
+//     client.query("select from unnest(enum_range(null::categ_produs))", function (err, rezCateg) {
+//         continuareQuery = ""
+//         res.locals.optiuni = rezCateg.rows;
+//         next();
+//     });
+//     // res.locals.utilizator = req.session.utilizator;
+//     //next();
+// })
+
 
 function createImages() {
     var continutFisier = fs.readFileSync(__dirname + "/resurse/json/galerie.json").toString("utf8");
@@ -64,6 +100,39 @@ app.get( ["/", "/index", "/home"], function(req, res) {
 
 app.get(["/despre", "despre"], function (req, res) {
     res.render("pagini/despre");
+});
+
+app.get("/produse",function(req, res){
+    console.log(req.query);
+    client.query("select * from unnest(enum_range(null::categ_prajitura))", function(err, rezCateg){
+        console.log(200)
+        continuareQuery=""
+        if (req.query.tip)
+            continuareQuery+=` and tip_produs='${req.query.tip}'` //"tip='"+req.query.tip+"'"
+        client.query("select * from prajituri where 1=1 " + continuareQuery , function( err, rez){
+            console.log(300)
+            if(err){
+                console.log(err);
+                renderError(res, 2);
+            }
+            else
+                res.render("pagini/produse", {produse:rez.rows, optiuni:rezCateg.rows});
+        });
+    });
+    console.log(100)
+});
+
+app.get("/produs/:id", function (req, res) {
+    client.query("select * from produse where id =" + req.params.id, function (err, rez) {
+        if (err) {
+            console.log(err);
+            renderError(2);
+        }
+        else {
+            res.render("pagini/produs", { prod: rez.rows[0] });
+            //console.log(rez);
+        }
+    })
 });
 
 app.get("/*.ejs", function (req, res) {
